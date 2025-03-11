d34e0f34207e494572f03a0b262121de83b07754
abhinavrana-17 <abhinavr078@gmail.com>
2025-03-11 09:59:28 +0530

    added libs and dependencies

---
+++ src/components/Chessboard.jsx
@@ -0,0 +1,14 @@
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+
+function ChessboardComponent() {
+    return (
+        <div>
+            <h2>simplyChess</h2>
+            <Chessboard />
+
+        </div>
+);
+}
+
+export default ChessboardComponent;
